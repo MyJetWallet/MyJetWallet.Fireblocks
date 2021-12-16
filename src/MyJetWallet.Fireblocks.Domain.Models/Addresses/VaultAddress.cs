d5fced860976c8b521ecd3ee1754b7deff9d83df
Aleksandr B <aleksandr.b@smplt.net>
2021-12-16 14:39:15 +0400

    fixed

---
+++ src/MyJetWallet.Fireblocks.Domain.Models/Addresses/VaultAddress.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace MyJetWallet.Fireblocks.Domain.Models.Addresses
+{
+    [DataContract]
+    public class VaultAddress
+    {
+        [DataMember(Order = 1)]
+        public string Address { get; set; }
+
+        [DataMember(Order = 2)]
+        public string LegacyAddress { get; set; }
+
+        [DataMember(Order = 3)]
+        public string EnterpriseAddress { get; set; }
+
+        [DataMember(Order = 4)]
+        public string Tag { get; set; }
+
+        [DataMember(Order = 5)]
+        public decimal Bip44AddressIndex { get; set; }
+    }
+}
