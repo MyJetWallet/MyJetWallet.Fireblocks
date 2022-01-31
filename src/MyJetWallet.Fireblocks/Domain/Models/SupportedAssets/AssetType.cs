221d3edce7e5346cf12b7055a284effb236a8c34
Aleksandr Belousov <aleksandr.b@smplt.net>
2022-01-31 12:06:25 +0400

    Add Supported Assets

---
+++ src/MyJetWallet.Fireblocks/Domain/Models/SupportedAssets/AssetType.cs
@@ -0,0 +1,14 @@
+﻿namespace MyJetWallet.Fireblocks.Domain.Models.SupportedAssets
+{
+    public enum AssetType
+    {
+        BASE_ASSET = 0,
+        ETH_CONTRACT = 1,
+        FIAT = 2,
+        ERC20 = 3,
+        BEP20 = 4,
+        COMPOUND = 5,
+        XLM_ASSET = 6,
+        UNKNOWN = 999
+    }
+}
---
+++ src/MyJetWallet.Fireblocks/Domain/Models/SupportedAssets/SupportedAsset.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyJetWallet.Fireblocks.Domain.Models.SupportedAssets
+{
+    [DataContract]
+    public class SupportedAsset
+    {
+        [DataMember(Order =1)]
+        public string Id { get; set; }
+
+        [DataMember(Order = 2)]
+        public string Name { get; set; }
+
+        [DataMember(Order = 3)]
+        public AssetType Type { get; set; }
+
+        [DataMember(Order = 4)]
+        public string ContractAddress { get; set; }
+
+        [DataMember(Order = 5)]
+        public string NativeAsset { get; set; }
+
+        [DataMember(Order = 6)]
+        public decimal Decimals { get; set; }
+    }
+}
